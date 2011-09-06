962f481864e83ef9976c1f62b7c1e91a5c41f0d7
steven reinisch <stevdev@web.de>
2011-09-06 11:45:50 +0200

    - EntityMappingPage has acceptable layout

--- eclipse_plugins/de.dubmas.modulob.migration.ui/src/de/dubmas/modulob/migration/ui/mapping/EntityMappingPage.java
+++ eclipse_plugins/de.dubmas.modulob.migration.ui/src/de/dubmas/modulob/migration/ui/mapping/EntityMappingPage.java
@@ -1,5 +1,7 @@
 package de.dubmas.modulob.migration.ui.mapping;
 
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -11,9 +13,12 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.xtend.util.stdlib.CloningExtensions;
 
+import de.dubmas.modulob.Entity;
+import de.dubmas.modulob.Feature;
 import de.dubmas.modulob.migration.FeatureChange;
 import de.dubmas.modulob.migration.Migration;
 import de.dubmas.modulob.migration.services.MigrationResult;
+import de.dubmas.modulob.migration.services.impl.Util;
 import de.dubmas.modulob.migration.ui.execcontext.ExecContext;
 import de.dubmas.modulob.migration.ui.execcontext.ExeccontextFactory;
 import de.dubmas.modulob.psee.ui.wizard.AbstractWizardPage;
@@ -33,7 +38,6 @@ public class EntityMappingPage extends AbstractWizardPage implements SelectionLi
 	private Button mustChangeButton;
 	
 	private Label currentFeatureLabel;
-	private Label featureCounterLabel;
 	
 	private ExecContext execContext;
 	
@@ -73,25 +77,70 @@ public class EntityMappingPage extends AbstractWizardPage implements SelectionLi
 	    featureMappingComp = new Composite(mainComposite, SWT.NONE);
 	    
 	    gl = new GridLayout();
-	    gl.numColumns = 2;
+	    gl.numColumns = 3;
 	    featureMappingComp.setLayout(gl);
 	    
-//	    GridData gridData            = new GridData();
-//	    gridData.horizontalSpan      = 2;
-//	    gridData.horizontalAlignment = SWT.FILL;
-//	    gridData.exclude             = true;
-//	    
-//	    featureMappingComp.setLayoutData(gridData);
-//	    featureMappingComp.setVisible(true);
+	    new Label (featureMappingComp, SWT.NONE).setText("Destination Feature:");
+	    new Label (featureMappingComp, SWT.NONE).setText("Source Entity:");
+	    new Label (featureMappingComp, SWT.NONE).setText("Source Feature:");
 	    
-	    new Label (featureMappingComp, SWT.NONE).setText("Feature:");
-	    currentFeatureLabel = new Label (featureMappingComp, SWT.NONE);
-	    currentFeatureLabel.setText("<aFeature>");
+	    GridData gridData = new GridData();
+	    gridData.horizontalSpan = 1;
+	    gridData.horizontalAlignment = GridData.FILL;
+	    gridData.grabExcessHorizontalSpace = true;
 	    
-	    featureCounterLabel = new Label (featureMappingComp, SWT.NONE);
-	    featureCounterLabel.setText("(x | y)");
+	    currentFeatureLabel   = new Label (featureMappingComp, SWT.NONE);
+	    currentFeatureLabel.setText("<aFeature> (x | y)");
+	    currentFeatureLabel.setLayoutData(gridData);
 	    
+	    gridData = new GridData();
+	    gridData.horizontalSpan = 1;
+	    gridData.horizontalAlignment = GridData.FILL;
+	    gridData.grabExcessHorizontalSpace = true;
 	    
+	    sourceEntityDropDown  = new Combo(featureMappingComp, SWT.DROP_DOWN);
+	    sourceEntityDropDown.setLayoutData(gridData);
+	    
+	    gridData = new GridData();
+	    gridData.horizontalSpan = 1;
+	    gridData.horizontalAlignment = GridData.FILL;
+	    gridData.grabExcessHorizontalSpace = true;
+	    
+	    sourceFeatureDropDown = new Combo(featureMappingComp, SWT.DROP_DOWN);
+	    sourceFeatureDropDown.setLayoutData(gridData);
+	    
+	    sourceEntityDropDown.addSelectionListener(this);
+	    sourceFeatureDropDown.addSelectionListener(this);   
+	}
+	
+	public void setVisible(boolean visible){
+		super.setVisible(visible);
+		
+		setState();
+		
+		/*
+		 * update
+		 */
+		 this.setTitle(this.getTitle() + "becoming visible ...");
+		 
+		 boolean isCopyChange = true;
+		 if(isCopyChange){
+			 copyOrChangeComposite.setVisible(true);
+			 featureMappingComp.setVisible(false);
+		 } else {
+			 copyOrChangeComposite.setVisible(false);
+			 featureMappingComp.setVisible(true);
+		 }
+		 
+		 mainComposite.layout(true);
+	}
+	
+	private void setState(){
+		setExecContext();
+		
+		List<String> entityNames = Util.namesFromEntities(execContext.getSourceEntities());
+	    String[] items           = entityNames.toArray(new String[entityNames.size()]);
+	    sourceEntityDropDown.setItems (items);
 	}
 	
 	private void setExecContext(){
@@ -181,26 +230,6 @@ public class EntityMappingPage extends AbstractWizardPage implements SelectionLi
 		execContext.setCurrentFeatureChange(currentFeatureChange);
 		execContext.setNextFeatureChangeIndex(featureChangeIndex + 1);
 	}
-	
-	public void setVisible(boolean visible){
-		super.setVisible(visible);
-		
-		/*
-		 * update
-		 */
-		 this.setTitle(this.getTitle() + "becoming visible ...");
-		 
-		 boolean isCopyChange = true;
-		 if(isCopyChange){
-			 copyOrChangeComposite.setVisible(true);
-			 featureMappingComp.setVisible(false);
-		 } else {
-			 copyOrChangeComposite.setVisible(false);
-			 featureMappingComp.setVisible(true);
-		 }
-		 
-		 mainComposite.layout(true);
-	}
 
 	@Override
 	public void widgetSelected(SelectionEvent e) {
@@ -216,14 +245,31 @@ public class EntityMappingPage extends AbstractWizardPage implements SelectionLi
 		if(e.getSource() == mustCopyButton ||
 		   e.getSource() == mustCopyButton)
 		{
-//			GridData gridData = (GridData)featureMappingComp.getLayoutData();
-//			gridData.exclude  = !gridData.exclude;
-//			featureMappingComp.setVisible(!gridData.exclude);
-//			
-//			featureMappingComp.layout(false);
-			
 			featureMappingComp.setVisible(!featureMappingComp.isVisible());
 			mainComposite.layout(true);
+		} 
+		else if(e.getSource() == sourceEntityDropDown)
+		{
+			int index = ((Combo)e.getSource()).getSelectionIndex();
+			if(index > -1)
+			{
+				Entity entity = execContext.getSourceEntities().get(index);
+				List<String> featureNames = Util.namesFromFeatures(entity.getFeatures());
+				String[] items = featureNames.toArray(new String[featureNames.size()]);
+			    sourceFeatureDropDown.setItems (items);
+			}
+			
+		} 
+		else if(e.getSource() == sourceFeatureDropDown)
+		{
+			int featureIndex = ((Combo)e.getSource()).getSelectionIndex();
+			if(featureIndex > -1)
+			{
+				int entityIndex = sourceEntityDropDown.getSelectionIndex();
+				Entity entity = execContext.getSourceEntities().get(entityIndex);
+				Feature f = entity.getFeatures().get(featureIndex);
+				//TODO: set feature as destinationFeature on current featureChange
+			}
 		}
 	}
 }
--- eclipse_plugins/de.dubmas.modulob.migration.ui/src/de/dubmas/modulob/migration/ui/mapping/EntityMappingWizard.java
+++ eclipse_plugins/de.dubmas.modulob.migration.ui/src/de/dubmas/modulob/migration/ui/mapping/EntityMappingWizard.java
@@ -10,8 +10,8 @@ public class EntityMappingWizard extends AbstractInteractiveActivityWizard{
 	public EntityMappingWizard() {
 		Map<String, String> pageMap = new HashMap<String, String>();
 		
-//		pageMap.put("1. Select source and destination", "de.dubmas.modulob.migration.ui.mapping.EntityModelsSelectionPage");
-//		pageMap.put("2. Create migration model", "de.dubmas.modulob.migration.ui.mapping.InvokeCompareAndTransform");
+		pageMap.put("1. Select source and destination", "de.dubmas.modulob.migration.ui.mapping.EntityModelsSelectionPage");
+		pageMap.put("2. Create migration model", "de.dubmas.modulob.migration.ui.mapping.InvokeCompareAndTransform");
 		pageMap.put("3. Map entity: ", "de.dubmas.modulob.migration.ui.mapping.EntityMappingPage");
 		
 		super.setPageMap(pageMap);
