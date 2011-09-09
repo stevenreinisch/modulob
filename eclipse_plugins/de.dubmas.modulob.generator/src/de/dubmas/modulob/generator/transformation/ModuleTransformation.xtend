bce786855581f027128273d162180f4e29a1a805
steven reinisch <stevdev@web.de>
2011-09-09 12:08:40 +0200

    - old entity models are read, too. - for each old entity model a corresponding DAO is generated

--- eclipse_plugins/de.dubmas.modulob.generator/src/de/dubmas/modulob/generator/transformation/ModuleTransformation.xtend
+++ eclipse_plugins/de.dubmas.modulob.generator/src/de/dubmas/modulob/generator/transformation/ModuleTransformation.xtend
@@ -1,11 +1,12 @@
 package de.dubmas.modulob.generator.transformation
 
-import java.util.List
+import java.util.*
 import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext
 import de.dubmas.modulob.system.Module
 import de.dubmas.modulob.system.InterfaceModel
 import de.dubmas.modulob.system.EntityModel
 import de.dubmas.modulob.system.NotificationModel
+import org.eclipse.xtend.util.stdlib.CloningExtensions
 
 class ModuleTransformation implements org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent {
 	
@@ -13,6 +14,7 @@ class ModuleTransformation implements org.eclipse.emf.mwe2.runtime.workflow.IWor
 	String entityModelSlotName
 	String notificationModelSlotName
 	String moduleSlotName
+	String oldVersionEntiyModelSlotName
 	
 	def setInterfaceModelSlotName(String name){
 		interfaceModelSlotName = name
@@ -30,25 +32,77 @@ class ModuleTransformation implements org.eclipse.emf.mwe2.runtime.workflow.IWor
 		moduleSlotName = name
 	}
 	
+	def setOldVersionEntiyModelSlotName(String name){
+		oldVersionEntiyModelSlotName = name
+	}
+	
 	override invoke(IWorkflowContext context) {
-		var List<Module> modules                       = context.get(moduleSlotName) as List<Module>
-		var List<InterfaceModel> interfaceModels       = context.get(interfaceModelSlotName) as List<InterfaceModel>
-		var List<EntityModel> entityModels             = context.get(entityModelSlotName) as List<EntityModel>
-		var List<NotificationModel> notificationModels = context.get(notificationModelSlotName) as List<NotificationModel>
-		
+		var modules            = context.get(moduleSlotName) as List<Module>
+		var interfaceModels    = context.get(interfaceModelSlotName) as List<InterfaceModel>
+		var entityModels       = context.get(entityModelSlotName) as List<EntityModel>
+		var notificationModels = context.get(notificationModelSlotName) as List<NotificationModel>
+	
 		for(m: modules){
 			m.interfaceModel = interfaceModels.filter(im | im.module == m).head
 		}
-	
+		
+		var groupedEntityModels    = sortedEntityModelsGroupedByModule(entityModels)
+		var oldVersionEntityModels = new ArrayList<EntityModel>(10);
+		
 		for(m: modules){
-			m.entityModel = entityModels.filter(em | em.module == m).head
+			var sortedEntityModels            = groupedEntityModels.get(m.name)
+			var entityModelWithHighestVersion = sortedEntityModels.remove(sortedEntityModels.lastKey)
+			
+			/*
+			 * For the remaining entity models we have to set the module reference and vice versa
+			 * to get the code generation work properly.
+			 * We clone the module because if we would use the original module the object graph
+			 * (semantic model, a.k.a. AST) would not have correct links.
+			 * We clone the module at this point because we do not want to copy the 
+			 * entityModelWithHighestVersion because it will be referenced later and the cloning
+			 * is a deep copy.
+			 */
+			for(em: sortedEntityModels.values){
+				var moduleClone = CloningExtensions::clone(m) as Module
+				moduleClone.entityModel = em
+				em.module = moduleClone
+			}
+			
+			m.entityModel = entityModelWithHighestVersion
+			
+			oldVersionEntityModels.addAll(sortedEntityModels.values)
 		}
+		
+		context.put(oldVersionEntiyModelSlotName, oldVersionEntityModels)
 		
 		for(m: modules){
 			m.notificationModel = notificationModels.filter(nm | nm.module == m).head
 		}
 	}
-
+	
+	def sortedEntityModelsGroupedByModule(List<EntityModel> entityModels){
+		/* 
+		 * map module names to sorted map of entity models 
+		 */
+		var Map<String, SortedMap<String, EntityModel>> moduleEntityModelsMap
+			= new HashMap<String, SortedMap<String, EntityModel>>();
+			
+		for(em: entityModels)
+		{
+			var SortedMap<String, EntityModel> entityModelsMap = 
+							moduleEntityModelsMap.get(em.module.name);
+							
+			if(entityModelsMap == null){
+				entityModelsMap = new TreeMap<String, EntityModel>()
+				moduleEntityModelsMap.put(em.module.name, entityModelsMap)
+			}
+			
+			entityModelsMap.put(em.version, em)
+		}
+		
+		return moduleEntityModelsMap
+	}
+	
 	override postInvoke() {
 		// TODO Auto-generated method stub
 		
--- eclipse_plugins/de.dubmas.modulob.generator/xtend-gen/de/dubmas/modulob/generator/transformation/ModuleTransformation.java
+++ eclipse_plugins/de.dubmas.modulob.generator/xtend-gen/de/dubmas/modulob/generator/transformation/ModuleTransformation.java
@@ -4,9 +4,16 @@ import de.dubmas.modulob.system.EntityModel;
 import de.dubmas.modulob.system.InterfaceModel;
 import de.dubmas.modulob.system.Module;
 import de.dubmas.modulob.system.NotificationModel;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent;
 import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+import org.eclipse.xtend.util.stdlib.CloningExtensions;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.ObjectExtensions;
@@ -21,6 +28,8 @@ public class ModuleTransformation implements IWorkflowComponent {
   private String notificationModelSlotName;
   
   private String moduleSlotName;
+  
+  private String oldVersionEntiyModelSlotName;
   
   public String setInterfaceModelSlotName(final String name) {
     String _interfaceModelSlotName = this.interfaceModelSlotName = name;
@@ -40,6 +49,11 @@ public class ModuleTransformation implements IWorkflowComponent {
   public String setModuleModelSlotName(final String name) {
     String _moduleSlotName = this.moduleSlotName = name;
     return _moduleSlotName;
+  }
+  
+  public String setOldVersionEntiyModelSlotName(final String name) {
+    String _oldVersionEntiyModelSlotName = this.oldVersionEntiyModelSlotName = name;
+    return _oldVersionEntiyModelSlotName;
   }
   
   public void invoke(final IWorkflowContext context) {
@@ -64,30 +78,73 @@ public class ModuleTransformation implements IWorkflowComponent {
         InterfaceModel _head = IterableExtensions.<InterfaceModel>head(_filter);
         m.setInterfaceModel(_head);
       }
+      Map<String,SortedMap<String,EntityModel>> _sortedEntityModelsGroupedByModule = this.sortedEntityModelsGroupedByModule(entityModels);
+      Map<String,SortedMap<String,EntityModel>> groupedEntityModels = _sortedEntityModelsGroupedByModule;
+      ArrayList<EntityModel> _arrayList = new ArrayList<EntityModel>(10);
+      ArrayList<EntityModel> oldVersionEntityModels = _arrayList;
       for (final Module m_1 : modules) {
-        final Function1<EntityModel,Boolean> _function_1 = new Function1<EntityModel,Boolean>() {
-            public Boolean apply(final EntityModel em) {
-              Module _module_1 = em.getModule();
-              boolean _operator_equals_1 = ObjectExtensions.operator_equals(_module_1, m_1);
+        {
+          String _name = m_1.getName();
+          SortedMap<String,EntityModel> _get_4 = groupedEntityModels.get(_name);
+          SortedMap<String,EntityModel> sortedEntityModels = _get_4;
+          String _lastKey = sortedEntityModels.lastKey();
+          EntityModel _remove = sortedEntityModels.remove(_lastKey);
+          EntityModel entityModelWithHighestVersion = _remove;
+          Collection<EntityModel> _values = sortedEntityModels.values();
+          for (final EntityModel em : _values) {
+            {
+              Object _clone = CloningExtensions.clone(m_1);
+              Module moduleClone = ((Module) _clone);
+              moduleClone.setEntityModel(em);
+              em.setModule(moduleClone);
+            }
+          }
+          m_1.setEntityModel(entityModelWithHighestVersion);
+          Collection<EntityModel> _values_1 = sortedEntityModels.values();
+          oldVersionEntityModels.addAll(_values_1);
+        }
+      }
+      context.put(this.oldVersionEntiyModelSlotName, oldVersionEntityModels);
+      for (final Module m_2 : modules) {
+        final Function1<NotificationModel,Boolean> _function_1 = new Function1<NotificationModel,Boolean>() {
+            public Boolean apply(final NotificationModel nm) {
+              Module _module_1 = nm.getModule();
+              boolean _operator_equals_1 = ObjectExtensions.operator_equals(_module_1, m_2);
               return ((Boolean)_operator_equals_1);
             }
           };
-        Iterable<EntityModel> _filter_1 = IterableExtensions.<EntityModel>filter(entityModels, _function_1);
-        EntityModel _head_1 = IterableExtensions.<EntityModel>head(_filter_1);
-        m_1.setEntityModel(_head_1);
+        Iterable<NotificationModel> _filter_1 = IterableExtensions.<NotificationModel>filter(notificationModels, _function_1);
+        NotificationModel _head_1 = IterableExtensions.<NotificationModel>head(_filter_1);
+        m_2.setNotificationModel(_head_1);
       }
-      for (final Module m_2 : modules) {
-        final Function1<NotificationModel,Boolean> _function_2 = new Function1<NotificationModel,Boolean>() {
-            public Boolean apply(final NotificationModel nm) {
-              Module _module_2 = nm.getModule();
-              boolean _operator_equals_2 = ObjectExtensions.operator_equals(_module_2, m_2);
-              return ((Boolean)_operator_equals_2);
+    }
+  }
+  
+  public Map<String,SortedMap<String,EntityModel>> sortedEntityModelsGroupedByModule(final List<EntityModel> entityModels) {
+    {
+      HashMap<String,SortedMap<String,EntityModel>> _hashMap = new HashMap<String,SortedMap<String,EntityModel>>();
+      Map<String,SortedMap<String,EntityModel>> moduleEntityModelsMap = _hashMap;
+      for (final EntityModel em : entityModels) {
+        {
+          Module _module = em.getModule();
+          String _name = _module.getName();
+          SortedMap<String,EntityModel> _get = moduleEntityModelsMap.get(_name);
+          SortedMap<String,EntityModel> entityModelsMap = _get;
+          boolean _operator_equals = ObjectExtensions.operator_equals(entityModelsMap, null);
+          if (_operator_equals) {
+            {
+              TreeMap<String,EntityModel> _treeMap = new TreeMap<String,EntityModel>();
+              entityModelsMap = _treeMap;
+              Module _module_1 = em.getModule();
+              String _name_1 = _module_1.getName();
+              moduleEntityModelsMap.put(_name_1, entityModelsMap);
             }
-          };
-        Iterable<NotificationModel> _filter_2 = IterableExtensions.<NotificationModel>filter(notificationModels, _function_2);
-        NotificationModel _head_2 = IterableExtensions.<NotificationModel>head(_filter_2);
-        m_2.setNotificationModel(_head_2);
+          }
+          String _version = em.getVersion();
+          entityModelsMap.put(_version, em);
+        }
       }
+      return moduleEntityModelsMap;
     }
   }
   
