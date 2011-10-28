4ae8af2208b50d08f9e956fa9e06692eb204cd27
steven reinisch <stevdev@web.de>
2011-10-28 17:12:15 +0200

    - added beautifier for xcdatmodels

---
+++ eclipse_plugins/de.dubmas.modulob.generator/src/de/dubmas/modulob/generator/templates/XCDataModelBeautifier.java
@@ -0,0 +1,9 @@
+package de.dubmas.modulob.generator.templates;
+
+public class XCDataModelBeautifier extends org.eclipse.xtend.typesystem.xsd.XMLBeautifier {
+	
+	public XCDataModelBeautifier () {
+		super();
+		fileExtensions = new String[] {"contents"};
+	}
+}
